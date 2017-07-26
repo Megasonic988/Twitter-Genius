382ad06f0363edba5385be820c18f9fe611475db
Joe Jimenez <t-jojime@microsoft.com>
2017-07-25 19:01:32 -0700

    twitter api added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Twitter = require('twitter');
+
 class App extends Component {
+  componentWillMount() {
+    console.log(Twitter)
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +16,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to lol.
         </p>
       </div>
     );
